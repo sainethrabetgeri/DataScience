6ad8079a0e5d9ebdab73c55292992f5eb4b070a7
Sai Nethra Betgeri <121192606+sainethra2710@users.noreply.github.com>
2023-01-06 16:40:44 -0600

    Add files via upload

--- RandomForsest_Class.py
+++ RandomForsest_Class.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jan  1 14:39:19 2023
+Created on Wed Jan  4 14:55:33 2023
 
 @author: nethrachekuri
 """
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 31 14:57:00 2022
 
-@author: nethrachekuri
-"""
+
 
 import numpy as np
 import pandas as pd
@@ -26,13 +21,11 @@ from sklearn.model_selection import train_test_split
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.25,random_state=0)
 
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-x_train = sc.fit_transform(x_train)
-x_test = sc.transform(x_test)
 
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors=3,weights='uniform',algorithm='auto',p=2 )
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=200,criterion="gini", max_depth=None)
+
 classifier.fit(x_train,y_train)
 
 y_pred=classifier.predict(x_test)
